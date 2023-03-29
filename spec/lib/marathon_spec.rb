43e2bc4f83798c4f79d804699cb614f61ffc1e4e
leonellcruz7 <leonellcruz111513@gmail.com>
2023-03-29 15:25:07 +0800

    submit

--- spec/lib/marathon_spec.rb
+++ spec/lib/marathon_spec.rb
@@ -27,10 +27,30 @@ describe MarathonLogs do
       # 1. create a spec when user create a log with a 
       # duration of 10 and distance of 8, and should shows a message:
       # "Duration: 10 hour/s, Distance: 8 km/s."
+      it "displays the correct message when logging a duration of 10 hrs and distance of 8 km" do
+        expect { subject.create_log(duration: 10, distance: 8) }.to output("Duration: 10 hour/s, Distance: 8 km/s.\n").to_stdout
+      end
     end
   end
 
   # 2. describe "#count_logs"
+  describe "#count_logs" do
+    context "when there are no logs" do
+      it "returns 0" do
+        expect(subject.count_logs).to eq 0
+      end
+    end
+
+    context "when there are logs" do
+      before do
+        3.times do
+          subject.create_log(duration: rand(10), distance: rand(8))
+        end
+      end
+
+      it "returns the correct count" do
+        expect(subject.count_logs).to eq 3
+      end
+    end
+  end
 end
-
-
